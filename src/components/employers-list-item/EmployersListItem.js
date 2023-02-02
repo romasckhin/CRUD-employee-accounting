ae4dd52be9306363c32eccef003a9c182a2f24c0
Roman <cikipuki10@gmail.com>
2023-02-02 21:04:10 +0200

    color changes by click class EmployersListItem

--- src/components/employers-list-item/EmployersListItem.js
+++ src/components/employers-list-item/EmployersListItem.js
@@ -1,26 +1,61 @@
 import './employers-list-item.css'
 import { DiCssTricks  } from "react-icons/di";
 import { BsFillTrashFill } from "react-icons/bs"
+import React, { Component } from 'react';
 
-const EmployersListItem = ({name, salary, increase}) => {
+
+class EmployersListItem extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.name = props.name
+        this.salary = props.salary
+        this.increase = props.increase
+
+        this.className = {
+            style: 'app-list-item'
+        }
+    }
+
+    changeIncrease = () => {
+
+        this.setState({
+            increase: this.increase = !this.increase
+        })
+
+        if (this.increase) {
+
+            this.setState({
+             style: this.className.style = 'app-list-item increase'
+            });
+
+        } else {
+            this.setState({
+                style: this.className.style = 'app-list-item'
+               });
+        }
+    }
 
 
-    return (
-        <li className='app-list-item' >
-            <div className='list-item-flex'>
-                <div className='item-name'>
-                    {name}      
+    render() {
+        return (
+            <li className={this.className.style} >
+                <div className='list-item-flex'>
+                    <div className='item-name'>
+                        {this.name}      
+                    </div>
+                    <div className='item-salary' >
+                        {this.salary}
+                    </div>
+                    <div className='item-btn'>
+                        <button title='Change content color' onClick={this.changeIncrease} className='btn-tricks'>{<DiCssTricks/>}</button>
+                        <button className='btn-trash'>{<BsFillTrashFill/>}</button>
+                    </div>
                 </div>
-                <div className='item-salary' >
-                    {salary}
-                </div>
-                <div className='item-btn'>
-                    <button className='btn-tricks'>{<DiCssTricks/>}</button>
-                    <button className='btn-trash'>{<BsFillTrashFill/>}</button>
-                </div>
-            </div>
-        </li>
-    );
-};
+            </li>
+        );
+    }
+}
 
 export default EmployersListItem;
